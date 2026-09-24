a5227694ea475cde83540faa018d1b8563654981
Jassim Ali <jassimalikk0@gmail.com>
2026-09-24 12:08:18 +0530

    small changes

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app=FastAPI()
 
+
+
+
 tasks=[{"id":1,"title":"Learn Python","Completed":False},
        {"id":2,"title":"Built To Do App","Completed":False}]
+
+
+class TaskCreate(BaseModel):       #creating a blueprint of data coming ,, to define the shape of data our API expects
+    title: str
+
+
 
 @app.get("/")
 def home():
@@ -14,8 +24,14 @@ def get_tasks():
     return tasks
 
 @app.post("/tasks")
-def create_tasks():
-    return {"message":"This is your new created tasks"}
+def create_tasks(task: TaskCreate):
+    new_task={
+        "id":len(tasks)+1,
+        "title":task.title,
+        "completed":False
+    }
+    tasks.append(new_task)
+    return new_task
 
 @app.put("/tasks")
 def update_tasks():
